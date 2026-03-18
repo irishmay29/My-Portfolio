80a76d8fe9ade99785f60102dc4ae828bdc0e511
irishmay29 <81654725+irishmay29@users.noreply.github.com>
2026-03-17 23:17:13 -1000

    fixed github pages issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://irishmay29.github.io',
+  base: '/My-Portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
